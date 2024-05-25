fd672eef2e31c8a1ece495d95dd744547fa03ca5
orvizz <153991700+orvizz@users.noreply.github.com>
2024-05-25 12:45:41 +0200

    Peticiones POST para crear recursos - Pujar y agregar items

--- routers/routerBids.js
+++ routers/routerBids.js
@@ -1,6 +1,30 @@
 let express = require("express")
 let routerBids = express.Router()
 let bids = require("../data/bids")
+let items = require("../data/items")
+
+routerBids.post("/", (req, res) => {
+    let amount = req.body.amount
+    let itemId = req.body.itemId
+    if( amount == undefined || isNaN(amount)){
+        return res.status(400).json({error: "Invalid amount"})
+    } 
+    if( itemId == undefined){
+        return res.status(400).json({error: "No item"})
+    }
+    let item = items.find(i => i.id == itemId)
+    if(item == undefined){
+        return res.status(400).json({error: "This item does not exist"})
+    }
+    let lastId = bids[bids.length-1].id
+    bids.push({
+        id: lastId+1,
+        userId: req.infoApiKey.id,
+        itemId: itemId,
+        amount: parseFloat(amount)
+    })
+    res.json({added: lastId+1})
+})
 
 routerBids.get("/", (req, res) => {
     res.json(bids)
--- routers/routerItems.js
+++ routers/routerItems.js
@@ -3,6 +3,21 @@ let routerItems = express.Router()
 
 let items = require("../data/items")
 
+routerItems.post("/", (req, res) => {
+    let description = req.body.description
+    if( description == undefined){
+        return res.status(400).json({error: "no description in body"})
+    }
+    let infoApiKey = req.infoApiKey
+    let lastId = items[items.length-1].id
+    items.push({
+        id: lastId+1,
+        userId: infoApiKey.id,
+        description: description
+    })
+    res.json({added: lastId+1})
+})
+
 routerItems.get("/", (req, res) => {
     res.json(items)
 })
