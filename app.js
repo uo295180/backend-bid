eb4b914ac53b5b1ab8491f623931710dccd21818
orvizz <153991700+orvizz@users.noreply.github.com>
2024-05-25 12:22:42 +0200

    middleware for items and bids

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 let express = require("express")
 let app = express()
 let port = 8081
+let jwt = require("jsonwebtoken")
 
 app.use(express.json())
+
+app.use(["/items", "/bids"], (req, res, next) => {
+    console.log("middleware")
+    let apiKey = req.query.apiKey
+    if(apiKey == undefined){
+        return res.status(401).json({error: "not apiKey"})
+    }
+    let infoApiKey = undefined
+    try{
+        infoApiKey = jwt.verify(apiKey, "secret")
+    } catch (error){
+        return res.status(401).json({error: "Invalid apiKey"})
+    }
+    if(infoApiKey == undefined){
+        return res.status(401).json({error: "Invalid apiKey"})
+    }
+    req.infoApiKey = infoApiKey
+    next()
+})
 
 let routerItems = require("./routers/routerItems")
 let routerBids = require("./routers/routerBids")
